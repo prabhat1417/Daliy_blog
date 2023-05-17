d7bfb967473997b6c0c501bb9b4f3889761502bb
prabhat1417 <106381748+prabhat1417@users.noreply.github.com>
2023-05-17 18:55:42 +0530

    init

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+require('dotenv').config()
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,13 +17,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public")); 
 
 
-// Run main function and catch error
 main().catch((err) => console.log(err));
 async function main() {
-  //localhost ain't working because in config it's binding to 127.0.0.1 
-  const url = "mongodb://127.0.0.1:27017"; 
-  const dbPath = "/blogDB";
-  await mongoose.connect(url + dbPath, {
+  const url = "mongodb+srv://"+ process.env.Password +"@cluster0.wystzfy.mongodb.net/blogDB"; 
+  await mongoose.connect(url, {
     useNewUrlParser: true,
   });
 
@@ -84,7 +82,7 @@ app.post("/compose", function (req, res) {
   // res.redirect("/compose");
 });
 
-app.listen(3000, function() {
+app.listen(3000 || process.env.PORT, function () {
   console.log("Server started on port 3000");
 });
 }
